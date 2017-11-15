2ff5eb5ea9da619da18338102e6b5e978eb349f3
Tzung-Chien Hsieh <la60312@gmail.com>
2017-11-15 18:56:36 +0100

    Add 1000G info to review page
    Add 1000G info from dbSNP page
    Add the result of query by position in ExAC

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,47 +1,79 @@
 class ReviewController < ApplicationController
 
   def index
+    require "open-uri"
     @pos = params[:chr]
     @snp_id = params[:snp]
     @ref = params[:ref]
-    @genotype = params[:genotype]
+      @genotype = params[:genotype]
     @annotation = params[:hgvs]
     # dbSNP
     dbsnp_prefix = "https://www.ncbi.nlm.nih.gov/projects/SNP/snp_ref.cgi?rs="
     @hgvs = []
     if @snp_id != ""
       @dbsnp_url = dbsnp_prefix + @snp_id.split("rs")[1]
-      require "open-uri"
       @data = URI.parse(@dbsnp_url).read
       doc = Nokogiri::HTML(@data)
       @hgvs = doc.at_css('[id="HGVS Names"]').css("li")
+      @cli_sig = doc.at_css('[id="Allele"]').css("td")[-4..-3]
+      @maf_title = doc.at_css('[id="Allele"]').css("td")[-2]
+      @maf = doc.at_css('[id="Allele"]').css("td")[-1].css('span')
+      puts @maf
     end
     
     # ExAC
-    exac_prefix = "http://exac.hms.harvard.edu/rest/dbsnp/"
+    # The first one is for REST, and the second one is for navigating to the website
+    exac_prefix = "http://exac.hms.harvard.edu/rest/"
     link_prefix = "http://exac.broadinstitute.org/dbsnp/"
-    @result_array = []
+
     pos = @pos.split(':')
-    ge = []
+
+    # Parse genotype
+    genotype = []
     if @genotype.include? "|"
-      ge = @genotype.split("|")
+      genotype = @genotype.split("|")
     end
     if @genotype.include? "/"
-      ge = @genotype.split("/")
+      genotype = @genotype.split("/")
     end
     alt = ''
-    ge.each do |g|
-      if g != @ref
-        alt = g
+    genotype.each do |value|
+      if value != @ref
+        alt =value 
       end
     end
-    #@exac_url = exac_prefix + pos[0] + '-' + pos[1] + '-' + @ref + '-' + alt
+
+    # Send request by position to ExAC 
+    exac_pos_url = exac_prefix + 'variant/'+ pos[0] + '-' + pos[1] + '-' + @ref + '-' + alt
     pos_prefix = "http://exac.broadinstitute.org/variant/"
     @exac_pos_link_url = pos_prefix + pos[0] + '-' + pos[1] + '-' + @ref + '-' + alt
-    @exac_link_url = link_prefix
+    url = URI.parse(exac_pos_url)
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+        http.request(req)
+    }
+    result = JSON.parse(res.body)['variant']
+    @pos_result_array = []
+    tmp = []
+    var_id = result['variant_id']
+
+    # @exac_pos_valid is for detecting if the mutation exsits in ExAC
+    @exac_pos_valid = true
+    if var_id.nil?
+      @exac_pos_valid = false
+    else
+      tmp.push(result['variant_id'])
+      tmp.push(result['filter'])
+      tmp.push(result['allele_count'])
+      tmp.push(result['allele_num'])
+      tmp.push(result['allele_freq'])
+      @pos_result_array.push(tmp)
+    end
+
+    @result_array = []
+    @exac_url = exac_prefix + 'dbsnp/'+ @snp_id
+    @exac_link_url = link_prefix + @snp_id
     if @snp_id != ""
-      @exac_url = exac_prefix + @snp_id
-      @exac_link_url = link_prefix + @snp_id
       url = URI.parse(@exac_url)
       req = Net::HTTP::Get.new(url.to_s)
       res = Net::HTTP.start(url.host, url.port) {|http|
