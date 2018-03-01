5a39c8bee0d07568b5d92273ad9751259e232772
Tzung-Chien Hsieh <la60312@gmail.com>
2018-03-01 14:13:56 +0100

    Remove uniqueness of submitter's name
    We use the email as unique identifier of submitter instead of name.
    Hence, we remove this constraint in submitter.
    Then, we can support one submitter with several emails.

--- app/models/submitter.rb
+++ app/models/submitter.rb
@@ -1,7 +1,7 @@
 class Submitter < ApplicationRecord
   has_many :patient
 
-  validates :last_name, uniqueness: true, presence: true
-  validates :first_name, uniqueness: true, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
 
 end
