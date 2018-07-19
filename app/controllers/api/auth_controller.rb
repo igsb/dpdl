c9118cebd5e89075eca4de7ebaf49bb13e30338d
Nanditha Mallesh <nanditha.mallesh@gmail.com>
2018-07-19 10:22:32 +0200

    formating the files for indentation

--- app/controllers/api/auth_controller.rb
+++ app/controllers/api/auth_controller.rb
@@ -1,10 +1,9 @@
 #Create a token, store it in db and send it back as a json object
 class Api::AuthController < Api::BaseController
-   
   def create
     key = ApiKey.create()
     respond_to do |format|
-	format.json { render plain: {token: key.access_token}.to_json, status: 200, content_type: 'application/json'}
-  end
+      format.json { render plain: {token: key.access_token}.to_json, status: 200, content_type: 'application/json'}
+    end
   end
 end
--- app/controllers/api/vcf_files_controller.rb
+++ app/controllers/api/vcf_files_controller.rb
@@ -1,80 +1,67 @@
 class Api::VcfFilesController < Api::BaseController
   before_action :authenticate_token
 
-   # POST /vcf_files
-   def create
-
+  # POST /vcf_files
+  def create
     fname = File.basename( params[:file].original_filename)
-
-    warnings = ""
-    alerts = ""
-    ActiveRecord::Base.transaction do
-      if fname =~ /zip$/  
-        tdir = File.join( VcfFile::VCF_PATH, fname )
-        zipfile = params[:file].tempfile.path
-        FileUtils.rmtree( tdir )    
-        Dir.mkdir( tdir, 0700 )
-        %x@unzip -aa -j "#{zipfile}" -d "#{tdir}"@ 
-
-        files = Dir.glob( File.join(tdir, "**") )
-        output = "#{tdir} \n\n"
-        files.each do |f|                                      
-          infile = File.open( f )
-          name = File::basename( f )
-          @vcf_file, w, a = VcfFile.add_file( infile, f, name, nil )
-          warnings << w
-          warnings << a
-        end
-        FileUtils.rmtree( tdir )
-      elsif fname =~ /gz$/ || fname =~ /txt$/  
-        f = "#{Rails.root}/#{VcfFile::VCF_TEMP}#{fname}"
-        FileUtils.cp( params[:file].tempfile.path, f )
-        e=%x@gunzip -d -f "#{f}"@
-        f.sub!(/\.gz$/,'')
-        infile = File.open( f )
-        name = File::basename( f )
-		puts infile
-		puts name
-		puts f
-        @vcf_file, warnings, alerts = VcfFile.add_file( infile, f, name, nil )
-        FileUtils.rm_f f       
-      else
-        infile = params[:file].open   
-        tempfile = params[:file].tempfile.path
-        @vcf_file, warnings, alerts = VcfFile.add_file( infile, tempfile, fname, nil )
-      end
-
-       
-      
-      if @vcf_file == nil
-	     respond_to do |format|
-		    format.json { render plain: {error:warnings}.to_json, status: 400, content_type: 'application/json'}
-		  end
+	warnings = ""
+	alerts = ""
+	ActiveRecord::Base.transaction do
+	  if fname =~ /zip$/  
+		tdir = File.join( VcfFile::VCF_PATH, fname )
+		zipfile = params[:file].tempfile.path
+		FileUtils.rmtree( tdir )    
+		Dir.mkdir( tdir, 0700 )
+		%x@unzip -aa -j "#{zipfile}" -d "#{tdir}"@ 
+		files = Dir.glob( File.join(tdir, "**") )
+		output = "#{tdir} \n\n"
+		files.each do |f|                                      
+		  infile = File.open( f )
+		  name = File::basename( f )
+		  @vcf_file, w, a = VcfFile.add_file( infile, f, name, nil )
+		  warnings << w
+		  warnings << a
+		end
+		FileUtils.rmtree( tdir )
+	  elsif fname =~ /gz$/ || fname =~ /txt$/  
+		f = "#{Rails.root}/#{VcfFile::VCF_TEMP}#{fname}"
+		FileUtils.cp( params[:file].tempfile.path, f )
+		e=%x@gunzip -d -f "#{f}"@
+		f.sub!(/\.gz$/,'')
+		infile = File.open( f )
+		name = File::basename( f )
+		@vcf_file, warnings, alerts = VcfFile.add_file( infile, f, name, nil )
+		FileUtils.rm_f f       
 	  else
-	    @vcf_file.create_samples
+		infile = params[:file].open   
+		tempfile = params[:file].tempfile.path
+		@vcf_file, warnings, alerts = VcfFile.add_file( infile, tempfile, fname, nil )
+	  end
+
+	  if @vcf_file == nil
+		respond_to do |format|
+		  format.json { render plain: {error:warnings}.to_json, status: 400, content_type: 'application/json'}
+		end
+	  else
+		@vcf_file.create_samples
 		@vcf_file.parse_vcf
 		@vcf_file.create_tabix
 		@vcf_file.name = @vcf_file.name + '.gz'
 		@vcf_file.save
-   
-	    respond_to do |format|
-		    format.json { render plain: {msg:"file uploaded successfully.PEDIA workflow triggered"}.to_json, status: 200, content_type: 'application/json'}
-		  end
-		end
-	   
-    end	  
-    
-    
-  end
-  
-  def authenticate_token
-    token = ApiKey.where(access_token: params[:token]).first
-    if token && !token.expired?
-		
-	else
 		respond_to do |format|
-			format.json { render plain: {error:'token invalid or expired.'}.to_json, status: 401, content_type: 'application/json'}
+		  format.json { render plain: {msg:"file uploaded successfully.PEDIA workflow triggered"}.to_json, status: 200, content_type: 'application/json'}
 		end
+	  end
+	end	  
+  end
+
+  def authenticate_token
+	token = ApiKey.where(access_token: params[:token]).first
+	if token && !token.expired?
+	else
+	  respond_to do |format|
+		format.json { render plain: {error:'token invalid or expired.'}.to_json, status: 401, content_type: 'application/json'}
+	  end
 	end
   end
 end
