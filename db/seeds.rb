db7f4e804aacc415e184cd76360b4f89d218633b
Nanditha Mallesh <nanditha.mallesh@gmail.com>
2018-08-21 14:47:05 +0200

    splitting into multiple lines

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,6 @@ case Rails.env
 when "development"
   user = User.create!(username: 'admin', password: 'admin123', password_confirmation: 'admin123', email: 'admin@gmail.com',
                       approved: 1, admin: 1, created_at: Time.now.to_datetime, updated_at: Time.now.to_datetime,
-					  first_name: 'admin', last_name: 'admin', institute: 'uni bonn', title: 'Dr.')
+                      first_name: 'admin', last_name: 'admin', institute: 'uni bonn', title: 'Dr.')
   user.save!
 end
