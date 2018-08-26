c856c34d9b3a3f2a391e7331a3dc5f4fafdb601b
Tzung-Chien <la60312@gmail.com>
2018-08-26 20:49:16 +0200

    Fix format issue in rspec

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "home/index", :type => :view do
-  it "render home" do
+  it 'render home' do
     render :template => "home/index.html.erb" 
-    expect(rendered).to include("News")
-    expect(rendered).to include("https://docs.google.com/presentation/")
+    expect(rendered).to include('News')
+    expect(rendered).to include('https://docs.google.com/presentation/')
   end
 end
