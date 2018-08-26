6a24a2ad447ab76d62eee4e1d86c956beb9763e0
Tzung-Chien <la60312@gmail.com>
2018-08-26 21:09:17 +0200

    Fix annotation model

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "home/index", :type => :view do
+RSpec.describe 'home/index', type: :view do
   it 'render home' do
-    render :template => "home/index.html.erb" 
+    render template: 'home/index.html.erb'
     expect(rendered).to include('News')
     expect(rendered).to include('https://docs.google.com/presentation/')
   end
