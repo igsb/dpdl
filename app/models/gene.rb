f70dd85461a23971c677adfc35bc6cc0cf6748e9
Tzung-Chien Hsieh <la60312@gmail.com>
2018-09-26 17:14:59 +0200

    Add script for remove duplicate gene

--- app/models/gene.rb
+++ app/models/gene.rb
@@ -3,5 +3,6 @@ class Gene < ApplicationRecord
   has_many :disorders, :through => :disorders_genes
   has_many :genes_mutations
   has_many :mutations_positions, :through => :genes_mutations
+  has_many :disorders_mutations_scores
   has_many :pedia
 end
