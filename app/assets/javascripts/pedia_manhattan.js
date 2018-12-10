a62724ecf4c04318d79f1424fce07b1fa8d6a692
Tzung-Chien <la60312@gmail.com>
2018-12-10 03:18:32 +0100

    Update precompile file

--- app/assets/javascripts/pedia_manhattan.js
+++ app/assets/javascripts/pedia_manhattan.js
@@ -15,21 +15,21 @@ function getChromosomeLabelByNum(num){
 function generateData(results, loweringFactor, chromosomeStartingPoint) {
 
 
-    results = results.map((result) => {
+    results = results.map(function(result) {
         result.location = (parseInt(result.pos)/loweringFactor) + chromosomeStartingPoint[result.chr - 1];
         return result;
     });
 
-    let dataSets = [
+    dataSets = [
         {
             label: 'NEUTRALS',
-            data: results.filter((result) => {return ((result.chr % 2) == 1)}),
+            data: results.filter(function(result) {return ((result.chr % 2) == 1)}),
             //data: filter(results , (result) => {return result.label === "0" && ((result.chromosome % 2) == 1)}),
             backgroundColor: '#C0C0C0',
         },
         {
             label: 'NEUTRALS',
-            data: results.filter((result) => {return ((result.chr % 2) == 0)}),
+            data: results.filter(function(result) {return ((result.chr % 2) == 0)}),
             //data: filter(results , (result) => {return result.label === "0" && ((result.chromosome % 2) == 0)}),
             backgroundColor: '#696969',
         },
@@ -44,14 +44,14 @@ function generateData(results, loweringFactor, chromosomeStartingPoint) {
     //    });
     //}
 
-    for(let i = 0; i < dataSets.length; i++){
+    for(i = 0; i < dataSets.length; i++){
         var x = []
         var y = []
         var pedia = []
         var gene_id = []
         var gene_symbol = []
         var color = []
-        for(let j = 0; j < dataSets[i].data.length; j++){
+        for(j = 0; j < dataSets[i].data.length; j++){
             data_point = dataSets[i].data[j];
             x.push(data_point.location);
             y.push(data_point.pedia_score);
@@ -81,11 +81,11 @@ function generateData(results, loweringFactor, chromosomeStartingPoint) {
 }
 loweringFactor = 1;
 chromosomeSizes = [249250621, 243199373, 198022430, 191154276, 180915260, 171115067, 159138663, 146364022, 141213431, 135534747, 135006516, 133851895, 115169878, 107349540, 102531392, 90354753, 81195210, 78077248, 59128983, 63025520, 48129895, 51304566, 155270560, 59373566, 16571];
-chromosomeSizes = chromosomeSizes.map((size) => {return size/loweringFactor});
+chromosomeSizes = chromosomeSizes.map(function(size) {return size/loweringFactor});
 chromosomeStartingPoint = [0];
 xLabels = [1];
-for(let i = 1 ; i < chromosomeSizes.length ; i++){
-    let startingPoint = (chromosomeSizes[i - 1]) + chromosomeStartingPoint[i - 1];
+for(i = 1 ; i < chromosomeSizes.length ; i++){
+    startingPoint = (chromosomeSizes[i - 1]) + chromosomeStartingPoint[i - 1];
     chromosomeStartingPoint.push(startingPoint);
     xLabels.push(getChromosomeLabelByNum(i + 1));
 }
