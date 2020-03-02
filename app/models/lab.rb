1661bad94f605590032e59716a18a0653f18b755
Tzung-Chien <la60312@gmail.com>
2020-03-02 18:26:43 +0100

    Use consumer for lab authentication
    Add consumer_id in labs table.
    Add consumer_id and external_id as unique index in labs table.

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -2,4 +2,6 @@ class Lab < ApplicationRecord
   has_many :patients
   has_many :labs_users
   has_many :users, :through => :labs_users
+  validates :lab_f2g_id, presence: true
+  validates :api_consumer_id, presence: true
 end
